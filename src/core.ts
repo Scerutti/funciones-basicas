b2f084b0a4539b8dc70ac9e5a3c379847c72c6d9
scerutti <softsys95@gmail.com>
2024-10-12 12:10:52 -0300

    fix package npm problems

--- src/core.ts
+++ src/core.ts
@@ -2,4 +2,5 @@ export { parseNumber } from "./parse-number.js";
 export { useWindowSize } from "./size-hook.js";
 export { toString } from "./toString.js";
 export { useLocalStorage } from "./useLocalStorage.js";
+export { toArray } from "./toArray.js";
 export * from "./functions.js";
